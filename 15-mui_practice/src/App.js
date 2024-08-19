9ef056c013fcc7811c9f2b0d0d74fb59bbba3187
yeon7513 <yeon7513@nate.com>
2024-08-19 14:37:24 +0900

    20240819

--- 15-mui_practice/src/App.js
+++ 15-mui_practice/src/App.js
@@ -1,7 +1,45 @@
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
 import './App.css';
 
 function App() {
-  return <div className="App">MUI Practice</div>;
+  return (
+    <div className="App">
+      <Container component="main" maxWidth="xs">
+        <Box>
+          <Typography component="h1" variant="h5">
+            회원가입
+          </Typography>
+          <Box component="form">
+            <FormControl component="fieldset">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    autoFocus
+                    fullWidth
+                    type="email"
+                    name="email"
+                    id="email"
+                    label="이메일 주소"
+                    autoComplete="off"
+                  />
+                </Grid>
+              </Grid>
+              <Button type="submit">회원가입</Button>
+            </FormControl>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
